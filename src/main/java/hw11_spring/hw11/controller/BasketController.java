6030a6488a3d5f4218c0d7ebb8ce6c148bf4d272
galina <galina.ramodina@gmail.com>
2023-12-20 15:43:28 +0300

    + package

--- src/main/java/hw11_spring/hw11/controller/BasketController.java
+++ src/main/java/hw11_spring/hw11/controller/BasketController.java
@@ -1,6 +1,6 @@
-package hw11_spring.hw11;
+package hw11_spring.hw11.controller;
 
-import org.springframework.context.annotation.Scope;
+import hw11_spring.hw11.service.BasketService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
--- src/main/java/hw11_spring/hw11/service/BasketService.java
+++ src/main/java/hw11_spring/hw11/service/BasketService.java
@@ -1,12 +1,18 @@
-package hw11_spring.hw11;
+package hw11_spring.hw11.service;
 
+import hw11_spring.hw11.Basket;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class BasketService {
-    Basket basket = new Basket();
+    private final Basket basket;
+
+    public BasketService(Basket basket) {
+        this.basket = basket;
+    }
+
     public void add (List<Integer> id){
         basket.getId().addAll(id);
     }
